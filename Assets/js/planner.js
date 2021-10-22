80cb7b66920560516fee9a465a2159fbc4b81077
jitasek <jitka.moreira@gmail.com>
2021-10-22 15:09:11 +0200

    set saveBtn + local storage

--- Assets/js/planner.js
+++ Assets/js/planner.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+  // above to make sure html and css get loaded first
   // display current day
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+  // click listener for saveBtn
+  $(".saveBtn").on("click", function () {
+    console.log(this);
+    //setting values of user input and time in local storage
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+  });
 });
